e863813a872c610c822938dcc56343880f3253cf
Mitch Downey <mitchldowney@gmail.com>
2020-09-03 08:07:18 -0500

    Move tab icons into tab-icons folder

--- src/resources/Images.ts
+++ src/resources/Images.ts
@@ -1,5 +1,5 @@
 export const Images = {
   BANNER: require('./images/banner.png'),
-  MORE: require('./images/tab-more.png'),
-  QUEUE: require('./images/tab-queue.png')
+  MORE: require('./images/tab-icons/tab-more.png'),
+  QUEUE: require('./images/tab-icons/tab-queue.png')
 }
