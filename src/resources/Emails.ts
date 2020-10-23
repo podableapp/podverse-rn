2701af0e77d60108e58c99b02daf9e096e7c91df
Mitch Downey <mitchldowney@gmail.com>
2020-10-23 18:12:44 -0500

    Update Emails.ts

--- src/resources/Emails.ts
+++ src/resources/Emails.ts
@@ -3,14 +3,14 @@ import Config from 'react-native-config'
 export const Emails = {
   CONTACT_US: {
     email: Config.CONTACT_US_EMAIL,
-    subject: 'Contact Podverse',
+    subject: 'Contact Podable',
     body:
       'If you are reporting an issue, please provide your device type ' +
       'and steps to reproduce the bug if possible. Thank you!'
   },
   CHECKOUT_ISSUE: {
     email: Config.SUPPORT_EMAIL,
-    subject: 'Podverse Checkout Issue',
+    subject: 'Podable Checkout Issue',
     body: `Please explain your issue below and we'll get back to you as soon as we can:`
   }
 }
