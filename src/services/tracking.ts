4c1b933aa5dd2c493a1a986cf280c747560e4ab4
Mitch Downey <mitchldowney@gmail.com>
2020-12-01 00:46:04 -0600

    Update podable tracking

--- src/services/tracking.ts
+++ src/services/tracking.ts
@@ -1,32 +1,35 @@
+import axios from 'axios'
 import { getGlobal } from 'reactn'
+import { generateQueryParams, getAppUserAgent } from '../lib/utility'
 import { gaTrackPageView } from './googleAnalytics'
 
 export const trackPageView = async (path: string, title: string) => {
   const global = getGlobal()
   const { player } = global
   const { nowPlayingItem = {} } = player
-  const { clipId, clipTitle, episodeId, episodeTitle, podcastId, podcastTitle } = nowPlayingItem
+  const { episodeTitle, podcastTitle } = nowPlayingItem
 
   const queryObj = {
     cd: title ? title : '',
     cg1: podcastTitle ? podcastTitle : '',
-    cg2: podcastId ? podcastId : '',
-    cg3: episodeTitle ? episodeTitle : '',
-    cg4: episodeId ? episodeId : '',
-    cg5: clipTitle ? clipTitle : '',
-    cg6: clipId ? clipId : ''
+    cg2: null, // episodeNumber
+    cg3: episodeTitle ? episodeTitle : ''
+    // cg4: orgID ? orgID : '',
+    // cg5: userid ? userid : '',
+    // uid: userid ? userid : ''
   }
 
   await gaTrackPageView(path, title, queryObj)
+  await podableTrackPageView(queryObj)
 }
 
 export const trackPlayerScreenPageView = (item: any, global: any) => {
-  if (item.clipId) {
-    trackPageView(
-      '/clip/' + item.clipId,
-      'Player Screen - Clip - ' + item.podcastTitle + ' - ' + item.episodeTitle + ' - ' + item.clipTitle
-    )
-  }
+  // if (item.clipId) {
+  //   trackPageView(
+  //     '/clip/' + item.clipId,
+  //     'Player Screen - Clip - ' + item.podcastTitle + ' - ' + item.episodeTitle + ' - ' + item.clipTitle
+  //   )
+  // }
   if (item.episodeId) {
     trackPageView(
       '/episode/' + item.episodeId,
@@ -35,5 +38,23 @@ export const trackPlayerScreenPageView = (item: any, global: any) => {
   }
   if (item.podcastId) {
     trackPageView('/podcast/' + item.podcastId, 'Player Screen - Podcast - ' + item.podcastTitle)
+  }
+}
+
+const podableTrackPageView = async (queryObj: any) => {
+  const endpoint = 'https://v3msw6f5o8.execute-api.us-west-2.amazonaws.com/dev'
+  const queryString = generateQueryParams(queryObj)
+  const userAgent = await getAppUserAgent()
+
+  try {
+    await axios({
+      url: `${endpoint}?${queryString}`,
+      method: 'GET',
+      headers: {
+        'User-Agent': userAgent
+      }
+    })
+  } catch (error) {
+    console.log('podableTrackPageView error', error)
   }
 }
